1e79173e6cc92857780acb8c41e9890b657e0287
ByeongHa <akdl4045@ajou.ac.kr>
2023-08-20 20:47:19 +0900

    index.html

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -13,7 +13,8 @@ public class HttpRequestUtils {
      *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
      * @return
      */
-    public static Map<String, String> parseQueryString(String queryString) {
+    public static Map<String, String> parseQueryString(String queryString)
+    {
         return parseValues(queryString, "&");
     }
 
